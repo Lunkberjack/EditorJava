f51f9e7405ae4c3a523fc79d6b1d021029cef6db
Lunkberjack <l.le.mi.lunk15@gmail.com>
2022-03-23 22:54:21 +0100

    Commit entregar

--- src/editor/EditorLuciaLM.java
+++ src/editor/EditorLuciaLM.java
@@ -1,7 +1,6 @@
 package editor;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,18 +24,25 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JToolBar;
 import javax.swing.text.DefaultEditorKit;
-
-public class EditorVideoIndio implements ActionListener {
+/**
+ * Clase que crea un editor simple de texto con las funciones de Abrir,
+ * Abrir Reciente, Guardar y crear un Archivo Nuevo. También se puede 
+ * Cortar, Copiar y Pegar desde un menú edición. Incorpora una ayuda.
+ * @author LucíaLM
+ * @versionm 1.0
+ */
+public class EditorLuciaLM implements ActionListener {
 	// No tiene por qué haber palabras, pero al menos hay una línea.
 	public int contadorRecientes, contadorPalabras = 0, contadorLineas = 1;
 	JFrame marco;
 	JMenuBar barraMenu;
-	JMenu archivo, herramientas;
+	JMenu archivo, edicion, ayuda;
 	JMenuItem archivoNuevo, archivoAbrir, archivoAbrirReciente, archivoGuardar,
-			  cortar, copiar, pegar;
+			  cortar, copiar, pegar, sobre;
 	JMenuItem reciente1, reciente2;
 	JToolBar barraHerramientas;
 	JTextPane areaTexto;
@@ -44,27 +50,31 @@ public class EditorVideoIndio implements ActionListener {
 	JPopupMenu menuPopup;
 	JButton botonNuevo, botonAbrir, botonRecientes, botonGuardar;
 	JPanel barraEstado;
-	JLabel cuentaPalabras, cuentaLineas;
+	JTextField nombreArchivo;
+	JLabel etiquetaArchivo, cuentaPalabras, cuentaLineas;
 
-	public EditorVideoIndio() {
+	public EditorLuciaLM() {
 		marco = new JFrame();
 		// Si se cierra la ventana de la GUI, el programa termina.
 		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		marco.setBounds(500, 200, 500, 500);
+		marco.setBounds(500, 200, 750, 500);
 		
-		// JTextPane
+		// JTextPane y JScrollPane
 		areaTexto = new JTextPane();
 		areaTexto.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		areaTexto.addKeyListener(new Teclas());
 		
-		// JScrollPane
 		scroll = new JScrollPane(areaTexto);
 		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		// Menú (barra)
 		barraMenu = new JMenuBar();
+		etiquetaArchivo = new JLabel("Nombre archivo: ");
+		nombreArchivo = new JTextField("Guardar con este nombre (cámbialo)", 10);
+		
 		archivo = new JMenu("Archivo");
-		herramientas = new JMenu("Herramientas");
+		edicion = new JMenu("Edición");
+		ayuda = new JMenu("Ayuda");
 
 		// Componentes del menú
 		archivoNuevo = new JMenuItem("Nuevo archivo");
@@ -85,6 +95,8 @@ public class EditorVideoIndio implements ActionListener {
 		cortar.addActionListener(new DefaultEditorKit.CutAction());
 		copiar.addActionListener(new DefaultEditorKit.CopyAction());
 		pegar.addActionListener(new DefaultEditorKit.PasteAction());
+		sobre = new JMenuItem("Sobre...");
+		sobre.addActionListener(this);
 
 		archivo.add(archivoNuevo);
 		archivo.add(archivoAbrir);
@@ -92,9 +104,10 @@ public class EditorVideoIndio implements ActionListener {
 		archivoAbrirReciente.add(reciente1);
 		archivoAbrirReciente.add(reciente2);
 		archivo.add(archivoGuardar);
-		herramientas.add(cortar);
-		herramientas.add(copiar);
-		herramientas.add(pegar);
+		edicion.add(cortar);
+		edicion.add(copiar);
+		edicion.add(pegar);
+		ayuda.add(sobre);
 
 		// Barra de herramientas
 		botonNuevo = new JButton();
@@ -143,18 +156,23 @@ public class EditorVideoIndio implements ActionListener {
 
 
 		// Se añaden varios componentes al marco
-
 		marco.add(barraHerramientas, BorderLayout.WEST);
 		marco.getContentPane().add(scroll, BorderLayout.CENTER);
 		barraMenu.add(archivo);
-		barraMenu.add(herramientas);
+		barraMenu.add(edicion);
+		barraMenu.add(ayuda);
+		barraMenu.add(etiquetaArchivo);
+		barraMenu.add(nombreArchivo);
 		marco.setJMenuBar(barraMenu);
 		marco.setTitle("Editor de LucíaLM");
+		
+		// Esta línea es muy importante. Sin ella no se visualiza el JFrame
+		// ni sus componentes.
 		marco.setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		EditorVideoIndio xd = new EditorVideoIndio();
+		EditorLuciaLM editor = new EditorLuciaLM();
 	}
 
 	@Override
@@ -165,11 +183,13 @@ public class EditorVideoIndio implements ActionListener {
 		if(evento.getSource() == archivoNuevo || evento.getSource() == botonNuevo) {
 			areaTexto.setText("");
 			marco.setTitle("Nuevo archivo");
+			nombreArchivo.setText("Pon un nombre");
 			contadorPalabras = 0;
-			/**
-			 * Abre el archivo de texto y lo muestra en el JTextPane.
-			 */
+		/**
+		 * Abre el archivo de texto y lo muestra en el JTextPane.
+		 */
 		} else if (evento.getSource() == archivoAbrir || evento.getSource() == botonAbrir) {
+			// Abre una ventana para elegir un archivo de nuestro sistema.
 			JFileChooser fileChooser = new JFileChooser();
 			int seleccion = fileChooser.showOpenDialog(areaTexto);
 			// Solo si se acepta:
@@ -178,11 +198,11 @@ public class EditorVideoIndio implements ActionListener {
 			} else {
 				// Si no se especifica un contenedor padre del recuadro de mensaje, este saldrá fuera
 				// del contenedor y confundirá a los usuarios.
-				JOptionPane.showMessageDialog(areaTexto, "No has seleccionado archivo :(");
+				JOptionPane.showMessageDialog(marco, "No has seleccionado archivo :(");
 			}
-			/**
-			 * Permite abrir hasta dos archivos recientemente abiertos.
-			 */
+		/**
+		 * Permite abrir hasta dos archivos recientemente abiertos.
+		 */
 		} else if(evento.getSource() == reciente1 || evento.getSource() == reciente2) {
 			String path;
 			if(evento.getSource() == reciente1) {
@@ -191,11 +211,10 @@ public class EditorVideoIndio implements ActionListener {
 				path = reciente2.getText();
 			}
 			abrirArchivo(path, areaTexto);
-			/**
-			 * Guarda el contenido en un archivo cuyo nombre hay que pasar como parámetro
-			 */
+		/**
+		 * Guarda el contenido en un archivo cuyo nombre hay que pasar como parámetro
+		 */
 		} else if (evento.getSource() == archivoGuardar || evento.getSource() == botonGuardar) {
-			JOptionPane.showMessageDialog(areaTexto,"No escribas nombre de archivo, tu archivo se llamará untitled.txt por defecto :)");
 			JFileChooser chooser = new JFileChooser(); 
 			chooser.setCurrentDirectory(new java.io.File("."));
 			chooser.setDialogTitle("Elija una carpeta para guardar el archivo: ");
@@ -204,24 +223,35 @@ public class EditorVideoIndio implements ActionListener {
 			// Si se acepta:
 			if (chooser.showOpenDialog(areaTexto) == JFileChooser.APPROVE_OPTION) { 
 				String path = chooser.getSelectedFile().getAbsolutePath();
-				guardarArchivo("untitled", path, areaTexto);
+				guardarArchivo(nombreArchivo.getText(), path, areaTexto);
 			}
 			else {
-				JOptionPane.showMessageDialog(areaTexto, "No has seleccionado carpeta :(");
+				JOptionPane.showMessageDialog(marco, "No has seleccionado carpeta :(");
 			}
-			/**
-			 * El botón de la barra de herramientas tiene una funcionalidad distinta:
-			 * en vez de presentar dos submenús, abre el archivo más reciente abierto.
-			 * Si no se encuentra el archivo, notifica al usuario.
-			 */
+		/**
+		 * El botón de la barra de herramientas tiene una funcionalidad distinta:
+		 * en vez de presentar dos submenús, abre el archivo más reciente abierto.
+		 * Si no se encuentra el archivo, notifica al usuario.
+		 */
 		} else if (evento.getSource() == botonRecientes) {
 			if(!reciente2.getText().equalsIgnoreCase("reciente 2")) {
 				abrirArchivo(reciente2.getText(), areaTexto);
 			} else if(!reciente1.getText().equalsIgnoreCase("reciente 1")) {
 				abrirArchivo(reciente1.getText(), areaTexto);
 			} else {
-				JOptionPane.showMessageDialog(areaTexto, "No tienes archivos recientes que abrir :(");
+				JOptionPane.showMessageDialog(marco, "No tienes archivos recientes que abrir :(");
 			}
+		/**
+		 * Mensaje de información y ayuda.
+		 */
+		} else if (evento.getSource() == sobre) {
+			JOptionPane.showMessageDialog(marco, "Hola :)\nEste editor puede crear, editar y guardar texto como archivo, "
+					+ "además de abrir archivos de tu sistema.\n\nNo olvides cambiar el nombre con el que quieres guardar"
+					+ " tu archivo, o este se guardará por defecto con uno de los nombres elegidos.\n\nEn el panel de abajo "
+					+ "podrás encontrar un conteo actualizado de palabras y líneas.\nSi ves que al copiar texto en el editor "
+					+ "no se actualiza, solo pulsa Espacio+Backspace y lo tendrás a tu disposición.\nRecuerda poner un espacio "
+					+ "tras cambiar de línea si solo vas a escribir una palabra por línea.\n\nPuedes editar desde "
+					+ "el menú Edición o haciendo click derecho sobre el área de texto.\n\n¡Disfruta!");
 		}
 	}
 	/**
@@ -244,12 +274,13 @@ public class EditorVideoIndio implements ActionListener {
 				contadorLineas++;
 			}
 			manejarRecientes(path);
+			// Muestra el número de líneas y palabras del archivo abierto.
 			contadorPalabras = areaTexto.getText().split("\\s+").length;
 			cuentaLineas.setText("Lineas: " + contadorLineas);
 			cuentaPalabras.setText("Palabras:" + contadorPalabras);
 			bfReader.close();
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(areaTexto,"No se encontró el archivo. ¿Lo has borrado o cambiado de directorio? :(");
+			JOptionPane.showMessageDialog(marco,"No se encontró el archivo. ¿Lo has borrado o cambiado de directorio? :(");
 			System.out.println("ERROR de entrada/salida. No se encontró el archivo.");
 		}
 	}
@@ -272,6 +303,9 @@ public class EditorVideoIndio implements ActionListener {
 	/**
 	 * Guarda el contenido del JTextPane en un archivo de texto. Si el archivo ya existe,
 	 * sobreescribe el contenido de este borrando lo anterior.
+	 * @param nombre
+	 * @param path
+	 * @param area
 	 */
 	public void guardarArchivo(String nombre, String path, JTextPane area) {
 		try {
@@ -282,11 +316,18 @@ public class EditorVideoIndio implements ActionListener {
 			bfWriter.write(area.getText());
 			bfWriter.close();
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(areaTexto,"Algo ha salido mal guardando tu archivo :(");
+			JOptionPane.showMessageDialog(marco,"Algo ha salido mal guardando tu archivo :(");
 			System.out.println("ERROR de entrada/salida.");
 		}
 	}
-
+	/**
+	 * Clase interna que permite el uso de KeyEvent (eventos de teclas) para
+	 * poder registrar la tecla pulsada y trabajar según los resultados.
+	 * Hereda de una clase abstracta que no puede ser instanciada, por ello 
+	 * la necesidad de crear una subclase que sí define su método abstracto.
+	 * @author LucíaLM
+	 * @version 1.0
+	 */
 	class Teclas extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent event) {
@@ -311,8 +352,6 @@ public class EditorVideoIndio implements ActionListener {
 				cuentaPalabras.setText("Palabras: " + contadorPalabras);
 				cuentaLineas.setText("Lineas: " + contadorLineas);
 			}
-			// Pruebas
-			// System.out.println(event.getKeyChar());
 		}
 	}
 }
